d043c7de47f8d44e62b8b02366314cfc0ae6265e
aymenomri <aymenomri2021@gmail.com>
2023-04-05 17:17:16 +0100

    new lesson

---
+++ video19.js
@@ -0,0 +1,19 @@
+let x ;
+
+
+const num = new Number(5);
+
+x = num.toString();
+x = num.toString().length;
+
+x = num.toFixed(2);
+x= num.toPrecision(2);
+x= num.toExponential(2);
+x = num.toLocaleString('ar-tn');
+x=num.valueOf();
+x = Number.MAX_VALUE;
+x=Number.MIN_VALUE;
+
+
+
+console.log(x);
