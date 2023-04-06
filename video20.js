2654667d71f7dfacc7633c51e4c50e84ff681f6c
aymenomri <aymenomri2021@gmail.com>
2023-04-06 01:45:36 +0100

    new work

---
+++ video20.js
@@ -0,0 +1,18 @@
+//console.log(Math);
+let x;
+
+x = Math.sqrt(10);
+x= Math.abs(85);
+
+x= Math.round(4.4);
+x= Math.ceil(4.9);
+x= Math.floor(4.9);
+x= Math.pow(3,3);
+x= Math.min(45,7);
+x= Math.max(45,7);
+x =Math.random(10);
+x=Math.floor(Math.random()*10+1);
+
+
+
+console.log(x);
