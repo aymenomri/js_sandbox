871ae88fef115d37a912fde281ef1b7777cd82dc
aymenomri <aymenomri2021@gmail.com>
2023-04-07 01:02:19 +0100

    new work

---
+++ number challengr.js
@@ -0,0 +1,40 @@
+const x = Math.floor(Math.random() *100 + 1);
+const y = Math.floor(Math.random() *50 + 1);
+
+//grt the sum
+const sum = x + y ;
+const sumOutput = `${x} + ${y} = ${sum}`;
+
+//console.log(x,y);
+console.log(sumOutput);
+
+
+//grt the difference 
+const diff = x - y ;
+const diffOutput = `${x} - ${y} = ${diff}`;
+
+
+console.log(diffOutput);
+
+//grt the product
+const prod = x - y ;
+const prodOutput = `${x} * ${y} = ${prod}`;
+
+
+console.log(prodOutput);
+
+
+
+//grt the quotient
+const quot = x / y ;
+const quotOutput = `${x} / ${y} = ${quot}`;
+
+
+console.log(quotOutput);
+
+//grt the remainder
+const rm = x % y ;
+const rmOutput = `${x} % ${y} = ${rm}`;
+
+
+console.log(rmOutput);
---
+++ video22.js
@@ -0,0 +1,20 @@
+let D;
+D = new Date();
+D = D.toDateString();
+
+D = new Date (1981,10,17,22,30,15);
+D = new Date ('2021-06-10T22:30:12');
+D = new Date ('10/7/2001 12:25:55');
+D = Date.now();
+D = new Date('10-10-2022 12:30:23');
+D = D.getTime();
+D = D.valueOf();
+D = new Date(1665401423000);
+D =Math.floor(Date.now() / 1000);
+
+
+
+
+
+console.log(D);
+//console.log(typeof D);
